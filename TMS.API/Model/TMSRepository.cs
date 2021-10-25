95aa4b9c0f711d978276844df9a0dabb0f9fbf20
Muhammad Faisal Maqsood <mfmlive@gmail.com>
2021-10-25 11:17:16 +0500

    Auto stash before merge of "main" and "origin/main"
    merging result

---
+++ TMS.API/Model/TMSRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TMS.API.Database;
+using TMS.Models.Model;
+
+namespace TMS.API.Model
+{
+    public interface ITMSRepository
+    {
+        Task<User> GetLogin(User user);
+    }
+
+    public class TMSRepository : ITMSRepository
+    {
+        private readonly TMSContext appdb;
+        public TMSRepository(TMSContext context)
+        {
+            appdb = context;
+        }
+
+        public async Task<User> GetLogin(User user)
+        {
+            User oUser = new User();
+            await Task.Run(() =>
+            {
+                oUser = new User();
+            });
+            return oUser;
+        }
+    }
+}
