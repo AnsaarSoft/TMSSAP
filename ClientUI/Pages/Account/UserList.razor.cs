15f3cea313529bb37061b052cf91f2f24bc84cae
Muhammad Faisal Maqsood <mfmlive@gmail.com>
2021-10-30 07:58:03 +0500

    user list working

---
+++ ClientUI/Pages/Account/UserList.razor.cs
@@ -0,0 +1,40 @@
+﻿using ClientUI.Services;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClientUI.Pages.Account
+{
+    public partial class UserList
+    {
+
+        #region Variable
+        bool flgBusy = false;
+        [Inject]
+        ITimeSheetServices oService { get; set; }
+        [Inject]
+        NavigationManager oNavigation { get; set; }
+        [Inject]
+        ISnackbar toast { get; set; }
+        #endregion
+
+        #region Function
+
+        public void SuccessMessage(string message)
+        {
+            toast.Add(message, Severity.Success);
+        }
+
+        public void ErrorMessage(string message)
+        {
+            toast.Add(message, Severity.Error);
+        }
+
+
+
+        #endregion
+    }
+}
